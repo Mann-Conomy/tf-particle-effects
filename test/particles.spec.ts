2e68a35db82283f686ff9340e78b0b22f8e5d69c
Simon Sørensen <fynboen1999@gmail.com>
2024-07-18 21:39:42 +0200

    Summer 2024

--- test/particles.spec.ts
+++ test/particles.spec.ts
@@ -110,7 +110,7 @@ describe("UnusualEffect", () => {
         const result = effect.all();
 
         // Assert
-        expect(result.length).toBe(450);
+        expect(result.length).toBe(503);
     });
 });
 
