6e8828abce729ac59686210b9e75e4ebc76bab58
Simon Sørensen <fynboen1999@gmail.com>
2024-07-20 13:46:52 +0200

    Revert previous dependency patch

--- src/resources/enums.ts
+++ src/resources/enums.ts
@@ -7,6 +7,39 @@ export enum InvalidParticle {
 }
 
 /**
+ * Supported language translations mapped to their corresponding Steam codes.
+ */
+export enum LanguageTranslation {
+    Ukrainian = "ukrainian",
+    Turkish = "turkish",
+    Thai = "thai",
+    TraditionalChinese = "tchinese",
+    Swedish = "swedish",
+    Spanish = "spanish",
+    SimplifiedChinese = "schinese",
+    Russian = "russian",
+    Romanian = "romanian",
+    Portuguese = "portuguese",
+    Polish = "polish",
+    Pirate = "pirate",
+    Norwegian = "norwegian",
+    Korean = "korean",
+    Japanese = "japanese",
+    Italian = "italian",
+    Hungarian = "hungarian",
+    Greek = "greek",
+    German = "german",
+    French = "french",
+    Finnish = "finnish",
+    English = "English",
+    Dutch = "dutch",
+    Danish = "danish",
+    Czech = "czech",
+    Bulgarian = "bulgarian",
+    Brazilian = "brazilian"
+}
+
+/**
  * Supported language subtags mapped to their corresponding language names.
  */
 export enum LanguageSubtag {
