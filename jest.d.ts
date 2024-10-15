d7aa00afec51e0134ecda86b9347d9b927056a14
Simon Sørensen <fynboen1999@gmail.com>
2024-10-15 15:45:38 +0200

    Simplify custom matchers

--- jest.d.ts
+++ jest.d.ts
@@ -1,12 +1,10 @@
 export type MathcerValue = number | string | boolean;
 
-declare global {
-    namespace jest {
-        interface Matchers<R> {
-            toAllBe(value: MathcerValue): void;
-        }
-        interface AsymmetricMatchers {
-            toAllBe(value: MathcerValue): void;
-        }
+declare module "expect" {
+    interface AsymmetricMatchers {
+        toAllBe(value: MathcerValue): void;
+    }
+    interface Matchers<R> {
+        toAllBe(value: MathcerValue): R;
     }
 }
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,31 +1,20 @@
 import { expect } from "@jest/globals";
 import type { MathcerValue } from "./jest";
 
-function toAllBe(actual: MathcerValue[], expected: MathcerValue) {
-    const pass = actual.every((value: MathcerValue) => value === expected);
-
-    if (pass) {
+expect.extend({
+    toAllBe(actual: MathcerValue[], expected: MathcerValue) {
+        const pass = actual.every((value: MathcerValue) => value === expected);
+    
+        if (pass) {
+            return {
+                pass: true,
+                message: () => `Expected all elements to not equal ${expected}`
+            }
+        }
+    
         return {
-            pass: true,
-            message: () => `Expected all elements to not equal ${expected}`
+            pass: false,
+            message: () => `Expected all elements to equal ${expected}, but received ${actual}`
         }
     }
-
-    return {
-        pass: false,
-        message: () => `Expected all elements to equal ${expected}, but received ${actual}`
-    }
-}
-
-expect.extend({
-    toAllBe
 });
-
-declare module "expect" {
-    interface AsymmetricMatchers {
-        toAllBe(value: MathcerValue): void;
-    }
-    interface Matchers<R> {
-        toAllBe(value: MathcerValue): void;
-    }
-}
